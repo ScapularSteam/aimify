4a554657f7284140264333bb82b332423ce9e1ae
Scapular Steam <86719656+ScapularSteam@users.noreply.github.com>
2024-06-26 20:08:18 +0100

    Added react hooks and created a card to visually seperate records

--- app/aimify/App.js
+++ app/aimify/App.js
@@ -1,19 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView} from 'react-native';
 import {Card} from './components/card'
+import { useState } from 'react';
 
 const titleTest = "Hello World!";
 const bodyTest = "Testing Testing, 123"
 
 export default function App() {
+
+  // Learning how to use react hooks to change states
+  const [record, updateRecord] = useState({date: 'test', trainingType: 'test', score: 0,})
+
   return (
+
+    
+
+
     <View style={styles.container}>
 
       <View style={styles.title}>
         <Text style={styles.titleText}>Aimify MVP</Text>
-      
       </View>
 
+      
+      <View style={styles.card}>
+          <Text style={styles.cardText}>{record.date}</Text>
+          <Text style={styles.cardText}>{record.trainingType}</Text>
+          <Text style={styles.cardText}>{record.score}</Text>
+      </View>
+
+      <View style={styles.card}>
+          
+      </View>
+      
+        
       
 
     <StatusBar hidden = {true}/>
@@ -28,14 +48,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#1f2123', // Same background colour as www.fletchworks.dev
     alignItems: 'center',
-    justifyContent: 'center',
+    
 
   },
 
   title: {
 
     // Anchor title bar to the top of the screen
-    position: 'absolute',
+    position: 'relative',
     top: 0,
 
     // Set dimensions of title bar
@@ -57,4 +77,22 @@ const styles = StyleSheet.create({
     fontSize: 22,
     color: 'white',
   },
+
+  card: {
+    backgroundColor: 'rgba(255, 255, 255, 0.023)',
+    borderRadius: 20,
+
+    height: '50%',
+    width: '90%',
+
+    marginTop: 50,
+
+  },
+
+  cardText: {
+    fontSize: 15,
+    color: 'white',
+  }
+
+
 });
